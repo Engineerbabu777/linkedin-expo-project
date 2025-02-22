92fb1d8f5f3e8c8975726c2e66fa4e48ad86a111
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-02-22 13:16:31 +0500

    `Added user registration, verification, and login api endpoints`

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
-
+const User = require("./models/User");
+const Post = require("./models/Post");
 const app = express();
 const port = 3000;
 const cors = require("cors");
@@ -28,4 +29,111 @@ mongoose
 
 app.listen(port, () => {
   console.log("Server is running on port 8000");
+});
+
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, profileImage } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      console.log("Email already registered");
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
+    const newUser = new User({
+      name,
+      email,
+      password,
+      profileImage,
+    });
+
+    newUser.verificationToken = crypto.randomBytes(20).toString("hex");
+
+    await newUser.save();
+
+    sendVerificationEmail(newUser.email, newUser.verificationToken);
+
+    res.status(202).json({
+      message:
+        "Registration successful.Please check your mail for verification",
+    });
+  } catch (error) {
+    console.log("Error registering user", error);
+    res.status(500).json({ message: "Registration failed" });
+  }
+});
+
+const sendVerificationEmail = async (email, verificationToken) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "sujananand0@gmail.com",
+      pass: "rnzcugnscqtqiefs",
+    },
+  });
+
+  const mailOptions = {
+    from: "linkedin@gmail.com",
+    to: email,
+    subject: "Email Verification",
+    text: `please click the following link to verify your email : http://localhost:3000/verify/${verificationToken}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Verification email sent successfully");
+  } catch (error) {
+    console.log("Error sending the verification email");
+  }
+};
+
+app.get("/verify/:token", async (req, res) => {
+  try {
+    const token = req.params.token;
+
+    const user = await User.findOne({ verificationToken: token });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid verification token" });
+    }
+
+    user.verified = true;
+    user.verificationToken = undefined;
+
+    await user.save();
+
+    res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Email verification failed" });
+  }
+});
+
+const generateSecretKey = () => {
+  const secretKey = crypto.randomBytes(32).toString("hex");
+
+  return secretKey;
+};
+
+const secretKey = generateSecretKey();
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, secretKey);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed" });
+  }
 });
